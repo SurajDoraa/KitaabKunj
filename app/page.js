e479e2a0f97175e6b0d3ea2c625f1176c64c8e17
Suraj <surajdor0548@gmail.com>
2024-07-27 20:39:55 +0530

    Linked signin and signup page with kitaabkunj

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
+
+
+
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
-    <Navbar />
+    <Navbar/>
     <Hero />
     <div className="relative">
       <About />
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,34 +1,27 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 import styles from '../styles';
-
 import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav 
-  variants={navVariants}
-  initial="hidden"
-  whileInView="show"
-  className={`${styles.xPaddings} py-8 relative`}>
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img 
-      src="/search.svg" 
-      alt="search"
-      className="w-[24px] h-[24px] object-contain"
-      />
-      <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
+    <div className={`${styles.innerWidth} mx-auto flex justify-between items-center relative`}>
+      <h2 className="absolute left-1/2 transform -translate-x-1/2 font-extrabold text-[24px] leading-[30px] text-white">
         KITAABKUNJ
       </h2>
-      <img
-      src="/menu.svg"
-      alt="menu"
-      className='w-[24px] h-[24px] object-contain'
-      />
-
-
+      <div className="ml-auto flex space-x-4">
+        <Link href="/signin" className="text-white hover:underline">Sign In</Link>
+        <Link href="/signup" className="text-white hover:underline">Sign Up</Link>
+      </div>
     </div>
   </motion.nav>
 );
